83863488f8225da854e35f5e86af2d6960ab7572
LORENZO SIGNORELLI <signolory@gmail.com>
2022-04-07 13:58:16 +0200

    Correzione clase

--- RemoteSpace/SpaceApi/Models/FileElementSample.cs
+++ RemoteSpace/SpaceApi/Models/FileElementSample.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,21 +7,21 @@ using System.Threading.Tasks;
 
 namespace SpaceApi
 {
-    public class FileElementSample
+    [Index(nameof(Name), nameof(FatherDirectory), nameof(IsDirectory), IsUnique =true)]
+    public class FileElement
     {
-        
-        public string Name { get; }
-        public decimal Weight { get; }
-        public string Onower { get; }
-        public DateTime UploadDate { get; }
-        public string path { get; }
-        public string Description { get; }
-        public bool Shared { get; }
-        public bool IsDirectory { get; }
-        public FileElementSample(string name, decimal weight, string owner,bool isdirectory=false, string path=null,string description=null, bool shared=false)
-        {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Weight { get; set; }
+        public string Onower { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public bool Shared { get; set; }
+        public bool IsDirectory { get; set; }
+        public FileElement FatherDirectory { get; set; }
 
-        }
-        
+
     }
 }
